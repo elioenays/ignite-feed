829b146eaac3278b820665fed7335cdc112d2e40
Elioenay <elionay407@gmail.com>
2023-03-28 12:02:03 -0300

    Estrutura da aplicação - Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,16 @@ export function Post() {
           <a href=''>#nodejs</a> <a href=''>#react</a>
         </p>
       </div>
+
+      <form className={styles.comentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder='Deixe um comentário' />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
