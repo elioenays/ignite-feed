eb7e7d4321cd493bc6c4082c31d13fd25aed5ce1
Elioenay <elionay407@gmail.com>
2023-03-28 11:01:59 -0300

    Estrutura da aplicação - Componente: Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,37 @@
+import styles from './Post.module.css'
+
 export function Post(props) {
   return (
-    <div>
-      <h1>{props.author}</h1>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src='https://github.com/elioenays.png'
+          />
+          <div className={styles.authorInfo}>
+            <strong>Eli</strong>
+            <span>Full Stack Developer</span>
+          </div>
+        </div>
+
+        <time
+          title='28 de Março as 10:51'
+          dateTime='2023-03-03 10:50:00'
+        >
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galera 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifólio.</p>
+        <p>
+          👉<a href=''>github.com/elioenays</a>
+        </p>
+        <p>
+          <a href=''>#novo projeto #rocketseat #nodejs #react</a>
+        </p>
+      </div>
+    </article>
   )
 }
