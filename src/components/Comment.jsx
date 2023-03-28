d12b61985d40b80964ee8c02ccd0b3e184d3953a
Elioenay <elionay407@gmail.com>
2023-03-28 13:12:11 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/elioenays.png' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Eli</strong>
+              <time
+                title='28 de Março as 10:51'
+                dateTime='2023-03-03 10:50:00'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Eli, parabéns!! 👏👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
